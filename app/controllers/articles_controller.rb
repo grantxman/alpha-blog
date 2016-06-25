6b4db766682102bf776bba71986e8d34baf75ae3
Grant Christman <foot01@gmail.com>
2016-06-25 17:24:02 +0000

    Add edit, update and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,42 @@
 class ArticlesController < ApplicationController
-  def new
-    @article = Article.new
-  end
+  
+def index
+  @articles = Article.all
+end
+
+  
+def new
+  @article = Article.new
+end
+  
+def show
+   @article = Article.find(params[:id])
+end
   
   def create
-    
-    #render plain:params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    
+    if @article.save
+      flash[:notice] = "Article was created"
+      redirect_to articles_path(@article)
+    else
+      render 'new'
+    end
   end
+  
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+  
   
   private 
   def article_params
